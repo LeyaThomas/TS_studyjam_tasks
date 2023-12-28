3af32d7c242811bfecf879dc77858cd83caddd10
Leya Thomas <96945948+LeyaThomas@users.noreply.github.com>
2023-12-28 09:50:19 +0530

    Update index.ts

--- Day1/index.ts
+++ Day1/index.ts
@@ -1,1 +1,22 @@
+function registerUser(name : string,birthYear : number){
+    const isAge=checkAge(birthYear)
+    if(isAge){
+        console.log("Registration Success!")
+        console.log(name+" has been registered successfully")
+    }
+    else{
+        console.log("Registration Failed")
+    }
+    
+}
+function checkAge(birthYear : number){
+    const currentYear=new Date().getFullYear()
+    const age= currentYear - birthYear
+    return age>=18
+    
 
+}
+
+registerUser('Leya', 2002);
+
+export{ }
